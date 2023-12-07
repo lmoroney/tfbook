1edec0f5375e36bca11066becc27efe392928d62
Jessica Tiberio <jessicatiberio18@gmail.com>
2023-12-06 22:34:59 -0500

    removed predict_classes and replaced with np.argmax

--- chapter8/irishsongs-generator.py
+++ chapter8/irishsongs-generator.py
@@ -29,7 +29,8 @@ next_words = 100
 for _ in range(next_words):
 	token_list = tokenizer.texts_to_sequences([seed_text])[0]
 	token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
-	predicted = model.predict_classes(token_list, verbose=0)
+	pred_classes = model.predict(token_list, verbose=0)
+	predicted = np.argmax(pred_classes, axis=-1)
 	output_word = ""
 	for word, index in tokenizer.word_index.items():
 		if index == predicted:
